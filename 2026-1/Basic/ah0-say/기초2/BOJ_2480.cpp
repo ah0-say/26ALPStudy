af35b809f1f21973e0e574980fe2e519db59f2e9
ah0-say <tladkdud06@gmail.com>
2026-03-24 20:01:50 +0900

    주사위세개

--- 2026-1/Basic/ah0-say/기초2/BOJ_2480.cpp
+++ 2026-1/Basic/ah0-say/기초2/BOJ_2480.cpp
@@ -1,2 +1,38 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    int a,b,c;
+    int cash;
+    cin >> a >> b >> c ;
+    if (a==b&&a==c)
+    {
+        cash = 10000+ a * 1000;
+    }
+    else if ((a==b&&a!=c)||(a==c&&a!=b))
+    {
+        cash= 1000+a*100;
+    }
+    else if (c==b&&a!=b)
+    {
+        cash = 1000+b*100;
+    }
+    else if (a!=b!=c)
+    {
+        if (a>b &&a>c)
+        {
+            cash=a*100;
+        }
+        else if (b>a && b>c)
+        {
+            cash=b*100;
+        }
+        else if (c>b && c>a)
+        {
+            cash=c*100;
+        }
+    }
+    cout<<cash;
+    return 0;
+}
